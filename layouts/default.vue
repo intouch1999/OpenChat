18a087105a53a22bf4cc9d08097368f018b6c9c1
intus su <ice.intouch.ice@gmail.com>
2024-10-18 21:22:21 +0700

    add rounded

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="navbar bg-primary flex justify-center items-end mb-8">
+    <div class="navbar bg-primary flex justify-center items-end mb-8 rounded-b-3xl">
     <div class=" absolute top-5 buttom-0 overflow-visible  rounded-full px-4 py-2 bg-primary z-50">
       <a class=" bg-primary rounded-full px-4 py-2 text-primary-content font-bold   text-xl">OpenChat</a>
     </div>
